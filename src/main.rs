792f3dea4f0e96f4d51ba29cb932cae918c2b47f
Felix <51097237+f321x@users.noreply.github.com>
2024-05-17 08:59:59 +0000

    testing bdk

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use dotenv::from_filename;
+#![allow(unused_variables)]
 use std::env;
+use bdk::{bitcoin::Network, database::MemoryDatabase, Wallet};
+
 
 // use bitcoin_utils::generate_address;
 // mod bitcoin_utils;
 // generate_address();
-enum Result<String, VarError> {
-    Ok(String),
-    Err(VarError),
+
+// enum Result<String, VarError> {
+//     Ok(String),
+//     Err(VarError),
+// }
+
+fn main() -> anyhow::Result<()> {
+
+    dotenv::from_filename(".env.example")?;
+
+    let descriptor = env::var("DESCRIPTOR")?;
+    // dbg!(descriptor);
+
+    let wallet = Wallet::new(
+        descriptor.as_str(),
+        None,
+        Network:: Signet,
+        MemoryDatabase::default())?;
+
+    dbg!(wallet);
+    Ok(())
 }
 
 
-fn main() {
-    from_filename(".env").ok();
-
-    let descriptor = env::var("DESCRIPTOR").unwrap();
-    dbg!(descriptor);
-}
-// https://www.youtube.com/watch?v=md-ecvXBGzI (31min)
+// https://www.youtube.com/watch?v=md-ecvXBGzI (63min)
